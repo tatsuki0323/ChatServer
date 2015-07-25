e0321dc5a356225c8dd449938c3eed473599f41b
niwatorihada <sugiharakouichirou@jazzs-MacBook-Air-2.local>
2015-07-25 17:39:36 +0900

    serverとhandlerの作成完了
    作成しました

---
+++ src/main/java/com/github/chatServer/ChatClientHandler.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatClientHandler extends Thread{
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    public ChatClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    /**
+     * 並列実行を行う時に事項されるメソッド
+     */
+     
+    public void run(){
+        try{
+            open();
+            while(true){
+                String message = receive();
+                if(message.equals("")) break;
+                send(message);
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally{
+            close();
+        }
+    }
+
+    /**
+     * クライアントとのデータのやり取りを行うストリームを開くメソッド
+     */
+    public void open() throws IOException{
+        in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream())
+        );
+        out = new BufferedWriter(
+            new OutputStreamWriter(socket.getOutputStream())
+        );
+    }
+    /**
+     * クライアントからデータを受け取るメソッド
+     */
+    public String receive() throws IOException{
+        String line = in.readLine();
+        System.out.println(line);
+        return line;
+    }
+
+    /**
+     * クライアントにデータを送信するメソッド
+     */   
+    public void send(String message) throws IOException{
+        out.write(message);
+        out.write("\r\n");
+        out.flush();
+    }
+
+    /**
+     * クライアントとの接続を閉じるメソッド
+     */
+    public void close(){
+        if(in != null){
+            try{
+                in.close();
+            } catch(IOException e){ }
+        }
+        if(out != null){
+            try{
+                out.close();
+            } catch(IOException e){ }
+        }
+        if(socket != null){
+            try{
+                socket.close();
+            } catch(IOException e){ }
+        }
+    }
+}
--- src/main/java/com/github/chatServer/ChatServer.java
+++ src/main/java/com/github/chatServer/ChatServer.java
@@ -1,1 +1,26 @@
-//ChatServer.javaの仮置きファイル
+import java.net.*;
+import java.io.*;
+
+public class ChatServer{
+    private ServerSocket server;
+    public void listen(){
+        try{
+            server = new ServerSocket(18080);
+            System.out.println("Echoサーバをポート18080で起動しました．");
+          
+            while(true){
+                Socket socket = server.accept();
+		    System.out.println("クライアントが接続してきました．");
+                ChatClientHandler handler = new ChatClientHandler(socket);
+                handler.start();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        ChatServer echo = new ChatServer();
+        echo.listen();
+    }
+}
